058b09f5927fedc3b959b4655fc84acbefc0f7de
Austin Wang <yyaustin@bu.edu>
2023-10-06 18:04:26 -0400

    Adjust styling details for Navbar and Home

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,63 +1,38 @@
 import { Link } from "react-router-dom";
-import { Navbar, Nav } from "react-bootstrap";
 import {
 	AiOutlineHome,
 	AiOutlineUser,
 	AiOutlineProject,
 	AiOutlineFileText,
 } from "react-icons/ai";
+import logo from "../assets/logo.png";
 
 const Navigation = () => {
-	// return (
-	// 	<Navbar bg="light" expand="lg" className="navbar">
-	// 		<Navbar.Brand as={Link} to="/">
-	// 			Austin Wang
-	// 		</Navbar.Brand>
-	// 		<Navbar.Toggle aria-controls="basic-navbar-nav" />
-	// 		<Navbar.Collapse id="basic-navbar-nav">
-	// 			<Nav className="ms-auto">
-	// 				<Nav.Link as={Link} to="/">
-	// 					<AiOutlineHome /> Home
-	// 				</Nav.Link>
-	// 				<Nav.Link as={Link} to="/about">
-	// 					<AiOutlineUser /> About
-	// 				</Nav.Link>
-	// 				<Nav.Link as={Link} to="/projects">
-	// 					<AiOutlineProject /> Projects
-	// 				</Nav.Link>
-	// 				<Nav.Link as={Link} to="/resume">
-	// 					<AiOutlineFileText /> Resume
-	// 				</Nav.Link>
-	// 			</Nav>
-	// 		</Navbar.Collapse>
-	// 	</Navbar>
-	// );
-
 	return (
 		<nav className="navbar">
 			<ul className="navbar__items">
-				<li className="navbar__logo">
-					<Link to="/" className="navbar__logo" id="logo">
-						Austin Wang
+				<li className="navbar__item">
+					<Link to="/" className="navbar__link">
+						<img src={logo} alt="Logo" className="navbar__logo" />
 					</Link>
 				</li>
 				<li className="navbar__item">
-					<Link to="/" className="navbar__link" id="home-page">
+					<Link to="/" className="navbar__link">
 						<AiOutlineHome /> Home
 					</Link>
 				</li>
 				<li className="navbar__item">
-					<Link to="/about" className="navbar__link" id="about-page">
+					<Link to="/about" className="navbar__link">
 						<AiOutlineUser /> About
 					</Link>
 				</li>
 				<li className="navbar__item">
-					<Link to="/projects" className="navbar__link" id="projects-page">
+					<Link to="/projects" className="navbar__link">
 						<AiOutlineProject /> Projects
 					</Link>
 				</li>
 				<li className="navbar__item">
-					<Link to="/resume" className="navbar__link" id="resume-page">
+					<Link to="/resume" className="navbar__link">
 						<AiOutlineFileText /> Resume
 					</Link>
 				</li>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,36 +1,27 @@
-import { Col, Container, Row } from "react-bootstrap";
 import homeMain from "../assets/home-main.svg";
 
 const Home = () => {
 	return (
 		<div className="home">
-			<Container>
-				<Row className="align-items-center">
-					<Col md={7}>
-						<h1 className="home__heading">
-							Hi There! <span className="home__wave">👋🏻</span>
-						</h1>
+			<div className="home__content">
+				<h1 className="home__heading">
+					Hi There! <span className="home__wave">👋🏻</span>
+				</h1>
 
-						<h1 className="home__heading">
-							I'm <span className="home__name">Austin Wang</span>
-						</h1>
+				<h1 className="home__heading">
+					I'm <span className="home__name">Austin Wang</span>
+				</h1>
 
-						<p className="home_roles">
-							MERN Stack Developer
-							<br />
-							Open Source Contributor
-						</p>
-					</Col>
+				<p className="home__description">
+					MERN Stack Developer
+					<br />
+					Open Source Contributor
+				</p>
+			</div>
 
-					<Col md={5}>
-						<img
-							src={homeMain}
-							alt="Coder is programming"
-							className="home__img"
-						/>
-					</Col>
-				</Row>
-			</Container>
+			<div className="home__img">
+				<img src={homeMain} alt="Coder is programming" />
+			</div>
 		</div>
 	);
 };
