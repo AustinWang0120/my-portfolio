31866e78dbd3057b821216db14b3fb21a709c1db
Austin Wang <yyaustin@bu.edu>
2023-10-06 15:29:19 -0400

    Populate Home content

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,36 @@
+import { Col, Container, Row } from "react-bootstrap";
+import homeMain from "../../assets/home-main.svg";
+
 const Home = () => {
 	return (
 		<div>
-			<h1>Home</h1>
+			<Container>
+				<Row>
+					<Col md={7}>
+						<h1>
+							Hi There! <span className="wave">👋🏻</span>
+						</h1>
+
+						<h1>
+							I'm <span className="name">Austin Wang</span>
+						</h1>
+
+						<p>
+							MERN Stack Developer
+							<br />
+							Open Source Contributor
+						</p>
+					</Col>
+
+					<Col md={5}>
+						<img
+							src={homeMain}
+							alt="Coder is programming"
+							style={{ maxHeight: "450px" }}
+						/>
+					</Col>
+				</Row>
+			</Container>
 		</div>
 	);
 };
