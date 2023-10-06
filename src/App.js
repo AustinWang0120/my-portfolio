19cfc73b325508c2f1cf47d6f3b6d3b8fed14809
Austin Wang <yyaustin@bu.edu>
2023-10-06 03:45:36 -0400

    Setup react-router-dom and major components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
+import Resume from "./components/Resume/Resume";
+
+const App = () => {
+	return (
+		<Router>
+			<div className="App">
+				<Route exact path="/" component={Home} />
+				<Route path="/about" component={About} />
+				<Route path="/projects" component={Projects} />
+				<Route path="/resume" component={Resume} />
+			</div>
+		</Router>
+	);
+};
 
 export default App;
