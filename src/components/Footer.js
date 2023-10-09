f45775afde1863c6c186cdf7166a6a641bfe49ef
Austin Wang <yyaustin@bu.edu>
2023-10-08 23:25:06 -0400

    Enhance styling for Home page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,25 @@
-import { Container, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
+import { AiFillGithub, AiFillLinkedin, AiFillMail } from "react-icons/ai";
 
 const Footer = () => {
 	return (
 		<Container>
-			<Row></Row>
+			<Row className="footer">
+				<Col md={4} className="footer__credits">
+					<p className="paragraph">Designed by Soumyajit Behera</p>
+					<p className="paragraph">Developed by Austin Wang</p>
+				</Col>
+
+				<Col md={4} className="footer__copyright">
+					<p className="paragraph">Copyright &copy; 2023 AW</p>
+				</Col>
+
+				<Col md={4} className="footer__icons">
+					<AiFillGithub size={40} className="footer__icon" />
+					<AiFillLinkedin size={40} className="footer__icon" />
+					<AiFillMail size={40} className="footer__icon" />
+				</Col>
+			</Row>
 		</Container>
 	);
 };
