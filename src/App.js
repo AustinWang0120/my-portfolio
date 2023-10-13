17bb145589f720a3c2782eff4e9b886d449ce23d
Austin Wang <yyaustin@bu.edu>
2023-10-13 14:17:07 -0400

    Add vercel analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -7,14 +8,17 @@ import Resume from "./pages/Resume";
 
 const App = () => {
 	return (
-		<Router>
-			<Routes>
-				<Route index element={<Home />} />
-				<Route path="about" element={<About />} />
-				<Route path="experience" element={<Experience />} />
-				<Route path="resume" element={<Resume />} />
-			</Routes>
-		</Router>
+		<div className="app">
+			<Router>
+				<Routes>
+					<Route index element={<Home />} />
+					<Route path="about" element={<About />} />
+					<Route path="experience" element={<Experience />} />
+					<Route path="resume" element={<Resume />} />
+				</Routes>
+			</Router>
+			<Analytics />
+		</div>
 	);
 };
 
